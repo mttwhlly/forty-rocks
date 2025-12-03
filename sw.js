b88d44e9d8d74432b10478f9e40851208b97d4c2
Matt Whalley <matt@mattwhalley.com>
2025-12-03 16:25:10 -0500

    Bump sw version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for NYC Birthday Adventure PWA
-const CACHE_NAME = 'nyc-birthday-v14';
+const CACHE_NAME = 'nyc-birthday-v15';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
